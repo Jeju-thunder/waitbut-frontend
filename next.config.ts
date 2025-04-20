650b9dbb5b29a00e04c738603a443658a156796d
Nami <69442847+namnameeroo@users.noreply.github.com>
2025-04-20 17:37:55 +0900

    feat: redirect 세팅 추가

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,20 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/login",
+        permanent: false,
+      },
+      {
+        source: "/kakao/redirect",
+        destination: "/auth",
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
